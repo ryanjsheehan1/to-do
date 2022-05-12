b8788ddc7e9048b26ef6d91d6638a37fa0db792c
Ryan Sheehan <83296615+ryanjsheehan1@users.noreply.github.com>
2022-05-12 11:06:21 -0500

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ryanjsheehan1/todo
+
+go 1.18
---
+++ todo.go
@@ -0,0 +1,105 @@
+package todo
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"time"
+)
+
+// item struct represents a ToDo item
+type item struct {
+	Task        string
+	Done        bool
+	CreatedAt   time.Time
+	CompletedAt time.Time
+}
+
+// List represents a list of ToDo items
+type List []item
+
+// Add creates a new ToDo item and appends it to the list
+func (l *List) Add(task string) {
+	t := item{
+		Task:        task,
+		Done:        false,
+		CreatedAt:   time.Now(),
+		CompletedAt: time.Time{},
+	}
+
+	*l = append(*l, t)
+}
+
+// Complete method marks a ToDo item as complted by
+// setting Done = true and CompletedAt to the current time
+func (l *List) Complete(i int) error {
+	ls := *l
+	if i <= 0 || i > len(ls) {
+		return fmt.Errorf("item %d does not exist", i)
+	}
+	// Adjusting index for 0 based index
+	ls[i-1].Done = true
+	ls[i-1].CompletedAt = time.Now()
+
+	return nil
+}
+
+// Delete method deletes a ToDo item from the list
+func (l *List) Delete(i int) error {
+	ls := *l
+	if i <= 0 || i > len(ls) {
+		return fmt.Errorf("item %d does not exist", i)
+	}
+	// Adjusting index for 0 based index
+	*l = append(ls[:i-1], ls[i:]...)
+
+	return nil
+}
+
+// Save method encodes the List as JSON and saves it
+// using the provided file name
+func (l *List) Save(filename string) error {
+	js, err := json.Marshal(l)
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(filename, js, 0644)
+}
+
+// Get method opens the provided filename, decodes the
+// JSON data and parses it into a List
+func (l *List) Get(filename string) error {
+	file, err := os.ReadFile(filename)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return nil
+		}
+		return err
+	}
+
+	if len(file) == 0 {
+		return nil
+	}
+
+	return json.Unmarshal(file, l)
+}
+
+// String prints out a formatted list
+// Implements the fmt.Stringer interface
+func (l *List) String() string {
+	formatted := ""
+
+	for k, t := range *l {
+		prefix := "  "
+		if t.Done {
+			prefix = "X "
+		}
+
+		// Adjust the item number k to print numbers starting from 1 instead of 0
+		formatted += fmt.Sprintf("%s%d: %s\n", prefix, k+1, t.Task)
+	}
+
+	return formatted
+}
